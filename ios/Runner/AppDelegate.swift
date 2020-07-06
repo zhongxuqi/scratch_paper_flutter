759e189ba732f1bec2bcbe0539195889d3b484d7
zhongxuqi <zhongxuqi@didichuxing.com>
2020-07-06 22:54:22 +0800

    add umeng

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,6 +9,10 @@ import AuthenticationServices
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        UMCommonSwift.initWithAppkey(appKey: "56ecff3ce0f55ac331000a80", channel: "ios")
+        UMAnalyticsSwift.setCrashReportEnabled(value: true)
+        UMAnalyticsSwift.setAutoPageEnabled(value: true)
+        
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,15 @@
 #import "GeneratedPluginRegistrant.h"
+
+#ifndef Runner_Bridging_Header_h
+#define Runner_Bridging_Header_h
+
+//导入UMCommon的OC的头文件
+#import <UMCommon/UMCommon.h>
+
+//导入UMCommonLog的OC的头文件
+#import <UMCommonLog/UMCommonLogManager.h>
+
+//导入UMAnalytics的OC的头文件
+#import <UMCommon/MobClick.h>
+
+#endif /* Runner_Bridging_Header_h */
